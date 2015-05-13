3883fdf08b37f5b772d46ae90dc86f44b9f6a2ea
Dan Bedard <danb@renci.org>
2015-05-13 02:34:02 -0400

    Setting hostname to public-hostname before setup_irods.sh to avoid resource configuration issues

--- ec2-irods4.0.3-idw2/per-instance/52-config.sh
+++ ec2-irods4.0.3-idw2/per-instance/52-config.sh
@@ -2,5 +2,6 @@
 
 # 52-config.sh
 # Configures iRODS using the response values in /opt/irods-aws/setup_responses
-
+sudo hostname `ec2metadata --public-hostname`
+sudo su -c "ec2metadata --public-hostname > /etc/hostname"
 sudo su -c "/var/lib/irods/packaging/setup_irods.sh < /opt/irods-aws/setup_responses"

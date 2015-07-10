71e42deb9c95ae04544f5e75a40abc0bfea1a649
Justin James <jjames@renci.org>
2015-07-10 14:33:20 -0400

    Changes for automatic creation of EC2 image.

--- ec2-irods4.0.3-idw2/per-once/build.sh
+++ ec2-irods4.0.3-idw2/per-once/build.sh
@@ -8,12 +8,13 @@ IRODS_VERSION=$2
 DB_PLUGIN_FOLDER=$3
 DB_PLUGIN_VERSION=$4
 
+
 sudo apt-get update
 sudo apt-get -y install postgresql openjdk-7-jdk
 sudo update-java-alternatives -s java-1.7.0-openjdk-amd64
 sudo apt-get -y install tomcat7 apache2
-wget -O /tmp/irods-icat.deb ftp://ftp.renci.org/pub/irods/releases/${IRODS_FOLDER}/irods-icat-${IRODS_VERSION}.deb
-wget -O /tmp/irods-postgres.deb ftp://ftp.renci.org/pub/irods/releases/${DB_PLUGIN_FOLDER}/irods-database-plugin-postgres-${DB_PLUGIN_VERSION}.deb
+wget -O /tmp/irods-icat.deb ftp://ftp.renci.org/pub/irods/releases/${IRODS_FOLDER}/ubuntu14/irods-icat-${IRODS_VERSION}-ubuntu14-x86_64.deb
+wget -O /tmp/irods-postgres.deb ftp://ftp.renci.org/pub/irods/releases/${DB_PLUGIN_FOLDER}/ubuntu14/irods-database-plugin-postgres-${DB_PLUGIN_VERSION}-ubuntu14-x86_64.deb
 sudo dpkg -i /tmp/irods-icat.deb /tmp/irods-postgres.deb
 sudo apt-get -f -y install
 #configure tomcat
---
+++ ec2-irods4.0.3-idw2/user_data_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt-get -y install git
+cd /opt
+sudo git clone https://github.com/irods/contrib
+mv /opt/contrib/ec2-irods4.0.3-idw2 /opt/irods-aws
+cd /opt/irods-aws
+
+# For 4.1.3
+./deploy.sh 4.1.3 4.1.3 4.1.3 1.5
+#sudo shred -u /etc/ssh/*_key /etc/ssh/*_key.pub
+#sudo shred -u /home/ubuntu/.ssh/authorized_keys
+sudo shred -u /home/ubuntu/.*history
+sudo shred -u /var/log/lastlog
+sudo shred -u /var/log/wtmp
+sudo touch /var/log/lastlog
+sudo touch /var/log/wtmp
+history -c
+

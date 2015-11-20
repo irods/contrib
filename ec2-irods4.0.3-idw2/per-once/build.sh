5d00dd47114c374f9c1b2d731c41cb475f09b2ff
Justin James <jjames@renci.org>
2015-11-20 17:18:27 -0500

    More changes for cloud browser.

--- ec2-irods4.0.3-idw2/per-once/build.sh
+++ ec2-irods4.0.3-idw2/per-once/build.sh
@@ -13,6 +13,7 @@ sudo apt-get update
 sudo apt-get -y install postgresql openjdk-7-jdk
 sudo update-java-alternatives -s java-1.7.0-openjdk-amd64
 sudo apt-get -y install tomcat7 apache2
+sudo apt-get install unzip
 wget -O /tmp/irods-icat.deb ftp://ftp.renci.org/pub/irods/releases/${IRODS_FOLDER}/ubuntu14/irods-icat-${IRODS_VERSION}-ubuntu14-x86_64.deb
 wget -O /tmp/irods-postgres.deb ftp://ftp.renci.org/pub/irods/releases/${DB_PLUGIN_FOLDER}/ubuntu14/irods-database-plugin-postgres-${DB_PLUGIN_VERSION}-ubuntu14-x86_64.deb
 sudo dpkg -i /tmp/irods-icat.deb /tmp/irods-postgres.deb

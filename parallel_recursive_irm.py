1693902db4a8a8a232d9eb5eff24d9db462e3fab
Terrell Russell <terrellrussell@gmail.com>
2019-05-09 23:24:17 -0400

    add graceful ssl handling

--- parallel_recursive_irm.py
+++ parallel_recursive_irm.py
@@ -6,6 +6,7 @@ import os
 import pprint
 import re
 import subprocess
+import ssl
 import sys
 
 try:
@@ -100,7 +101,9 @@ except KeyError:
     env_file = os.path.expanduser('~/.irods/irods_environment.json')
 
 # get connection
-with iRODSSession(irods_env_file=env_file) as session:
+ssl_context = ssl.create_default_context(purpose=ssl.Purpose.SERVER_AUTH, cafile=None, capath=None, cadata=None)
+ssl_settings = {'ssl_context': ssl_context}
+with iRODSSession(irods_env_file=env_file, **ssl_settings) as session:
     try:
         # get clean logical path
         lp = args.logical_path.rstrip('/')

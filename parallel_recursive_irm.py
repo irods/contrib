d1e31273d2819f3664826b6da6ab08c36c6f0f9c
Terrell Russell <terrellrussell@gmail.com>
2019-05-09 21:10:19 -0400

    updates for parallel_recursive_irm.py
    - removed starting_depth argument, will recurse to the bottom
    - added operation argument, now can be unregister, trash, or force
    - escaped known-bad-in-bash characters
    - changed command-files to have .sh extension
    - added elapsed time
    - better error messages

--- parallel_recursive_irm.py
+++ parallel_recursive_irm.py
@@ -7,31 +7,35 @@ import pprint
 import subprocess
 import sys
 
-from irods.exception import CollectionDoesNotExist
-from irods.session import iRODSSession
+try:
+    from irods.exception import CollectionDoesNotExist
+    from irods.session import iRODSSession
+except ImportError as e:
+    print('ERROR: python-irodsclient module is missing, try:')
+    print('  pip install python-irodsclient')
+    sys.exit(1)
+
 
 def log(x):
     if (args.verbose == True):
         pp = pprint.PrettyPrinter(indent=4)
         pp.pprint(x)
 
-def add_to_tree(tree, collection, level, max_depth):
+def add_to_tree(tree, collection, level):
     if level not in tree:
         tree[level] = set()
     # add collection
     tree[level].add(collection)
-    # dig to max_depth
-    if level < max_depth:
-        # add any subcollections
-        for c in collection.subcollections:
-            tree = add_to_tree(tree, c, level + 1, max_depth)
+    # add any subcollections
+    for c in collection.subcollections:
+        tree = add_to_tree(tree, c, level + 1)
     return tree
 
 def produce_commands_for_tree(tree, operation):
     # prepare target command directory
     cmd_dir = 'irm_' + operation + '_' + datetime.datetime.now().strftime('%Y%m%dT%H%M%S')
     os.mkdir(cmd_dir)
-    log(os.path.realpath(cmd_dir))
+    log(['command directory', os.path.realpath(cmd_dir)])
     # prepare command string
     if operation == 'unregister':
         cmd = 'irm -r -U "{0}"\n'
@@ -44,29 +48,44 @@ def produce_commands_for_tree(tree, operation):
         sys.exit(1)
     # walk tree
     for i in tree:
-        filename = '{0}/{1}.txt'.format(cmd_dir, i)
+        filename = '{0}/{1}.sh'.format(cmd_dir, i)
         with open(filename, 'w') as f:
             for c in tree[i]:
-                f.write(cmd.format(c.path))
+                escaped_path = c.path.replace('$','\$').replace('"','\\"')
+                f.write(cmd.format(escaped_path))
     return cmd_dir
 
 def run_commands(cmd_dir):
     # run files
     for i in sorted(os.listdir(cmd_dir), reverse=True):
         cmd_file = os.path.join(cmd_dir, i)
-        log('running {0}...'.format(cmd_file))
-        subprocess.Popen(['parallel'], stdin=open(cmd_file), shell=False).communicate()
+        log(['running...', cmd_file])
+        try:
+            subprocess.Popen(['parallel'], stdin=open(cmd_file), shell=False).communicate()
+        except OSError as e:
+            if e.errno == os.errno.ENOENT:
+                print('ERROR: gnu parallel not found in your $PATH')
+                sys.exit(1)
+            else:
+                raise
+
+########################
+
+start_time = datetime.datetime.now()
 
 d = '''Parallel Recursive Removal of a Logical Path
 
+- Requires GNU Parallel
+- Requires irm
+
 - Available Operations:
-  - 'unregister' (default)
-  - 'trash' (move to the trash)
-  - 'force' (unlink from storage)
+  - 'unregister' (only remove from the catalog)
+  - 'trash' (move to the trash, in both catalog and storage)
+  - 'force' (really remove, from both catalog and storage)
 '''
 parser = argparse.ArgumentParser(description=d, formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument('logical_path', help='Absolute Logical Path (Collection)')
-parser.add_argument('starting_depth', help='Starting Depth for Parallelism', type=int)
+parser.add_argument('operation', help='Operation Mode for irm', choices=['unregister', 'trash', 'force'])
 parser.add_argument('-v', '--verbose', help="print more information to stdout", action="store_true")
 args = parser.parse_args()
 
@@ -76,20 +95,25 @@ try:
 except KeyError:
     env_file = os.path.expanduser('~/.irods/irods_environment.json')
 
-# connect and apply catalog filesystem metadata to collection
+# get connection
 with iRODSSession(irods_env_file=env_file) as session:
     try:
+        # get clean logical path
         lp = args.logical_path.rstrip('/')
         coll = session.collections.get(lp)
         log(['target_collection_path', coll.path])
-        log(['starting_depth', args.starting_depth])
         # build the tree
-        tree = add_to_tree({}, coll, 0, args.starting_depth)
-        log(tree)
+        tree = add_to_tree({}, coll, 0)
+        log(['tree',tree])
         # produce directory of files with commands
-        cmd_dir = produce_commands_for_tree(tree, 'unregister')
+        log(['operation', args.operation])
+        cmd_dir = produce_commands_for_tree(tree, args.operation)
         # run commands
         run_commands(cmd_dir)
     except CollectionDoesNotExist:
         print('logical path ['+lp+'] does not exist')
         sys.exit(1)
+
+end_time = datetime.datetime.now()
+elapsed_time = end_time - start_time
+print('elapsed time: '+datetime.datetime.utcfromtimestamp((elapsed_time).total_seconds()).strftime("%H:%M:%S"))
